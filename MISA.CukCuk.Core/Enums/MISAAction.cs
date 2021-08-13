d59d858e4bec2e07a62ef4e0ceb3a69a128d6f23
HieuPham2000 <hieu.pt183535@gmail.com>
2021-08-13 10:20:19 +0700

    add: misa action constant

---
+++ MISA.CukCuk.Core/Enums/MISAAction.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MISA.CukCuk.Core.Enums
+{
+    /// <summary>
+    /// Kiểu hành động
+    /// </summary>
+    /// CreatedBy: PTHIEU (04/08/2021)
+    public enum MISAAction
+    {
+        /// <summary>
+        /// Hành động lấy tất cả dữ liệu
+        /// </summary>
+        GetAll = 0,
+
+        /// <summary>
+        /// Hành động lấy dữ liệu 
+        /// </summary>
+        GetById = 1,
+
+        /// <summary>
+        /// Hành động thêm dữ liệu mới
+        /// </summary>
+        Insert = 2,
+
+        /// <summary>
+        /// Hành động cập nhật/chỉnh sửa dữ liệu
+        /// </summary>
+        Update = 3,
+
+        /// <summary>
+        /// Hành động xóa dữ liệu
+        /// </summary>
+        Delete = 4
+    }
+}
