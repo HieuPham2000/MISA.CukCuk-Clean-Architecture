b30e2f0e05f8d8ff9fb9e9a6007e19cf0079b698
HieuPham2000 <hieu.pt183535@gmail.com>
2021-08-13 10:24:17 +0700

    modify: use stored procedure command

--- MISA.CukCuk.Infrastructure/Repositories/BaseRepository.cs
+++ MISA.CukCuk.Infrastructure/Repositories/BaseRepository.cs
@@ -1,4 +1,5 @@
 ﻿using Dapper;
+using Microsoft.Extensions.Configuration;
 using MISA.CukCuk.Core.Interfaces.Infrastructure;
 using MySqlConnector;
 using System;
@@ -10,6 +11,11 @@ using System.Threading.Tasks;
 
 namespace MISA.CukCuk.Infrastructure.Repositories
 {
+    /// <summary>
+    /// Lớp cơ sở cho việc thao tác với CSDL (truy vấn, thêm, sửa, xóa... dữ liệu)
+    /// </summary>
+    /// <typeparam name="MISAEntity">Lớp thực thể</typeparam>
+    /// CreatedBy: PTHIEU (30-07-2021)
     public class BaseRepository<MISAEntity>: IBaseRepository<MISAEntity> where MISAEntity: class
     {
         #region Fields
@@ -17,36 +23,31 @@ namespace MISA.CukCuk.Infrastructure.Repositories
         /// <summary>
         /// Thông tin về kết nối
         /// </summary>
-        string _connectionString;
+        string _connectionString = string.Empty;
 
         /// <summary>
         /// Tên lớp entity
         /// </summary>
-        string _entityClassName;
+        protected string TableName = string.Empty;
 
         /// <summary>
         /// Kết nối tới cơ sở dữ liệu
         /// </summary>
-        protected IDbConnection DbConnection;
+        protected IDbConnection DbConnection = null;
 
         #endregion
 
         #region Constructor
-        public BaseRepository()
+        public BaseRepository(IConfiguration configuration)
         {
-            // Khai báo thông tin kết nối:
-            _connectionString = "" +
-                "Host=47.241.69.179;" +
-                "Port=3306;" +
-                "Database=MISA.CukCuk_Demo_NVMANH;" +
-                "User Id=dev;" +
-                "Password=12345678";
+            // lấy ra thông tin kết nối:
+            _connectionString = configuration.GetConnectionString("MISACukCukConnectionString");
 
             // Khởi tạo kết nối:
             DbConnection = new MySqlConnection(_connectionString);
 
-            // Lấy ra tên lớp entity
-            _entityClassName = typeof(MISAEntity).Name;
+            // Lấy ra tên lớp entity (tên bảng)
+            TableName = typeof(MISAEntity).Name;
         }
         #endregion
 
@@ -57,17 +58,13 @@ namespace MISA.CukCuk.Infrastructure.Repositories
         /// </summary>
         /// <returns>Danh sách đối tượng (List object)</returns>
         /// CreatedBy: PTHIEU (30-07-2021)
-        public List<MISAEntity> GetAll()
+        /// ModifiedBy: PTHIEU (02-08-2021)
+        /// Sử dụng Stored Procedure thay cho câu lệnh sql dạng text
+        public IEnumerable<MISAEntity> GetAll()
         {
-
-            // Khai báo lệnh sql
-            var sqlCommand = $"SELECT * FROM {_entityClassName}";
-
-            // Thực hiện truy vấn dữ liệu với Dapper
-            var entities = DbConnection.Query<MISAEntity>(sqlCommand).ToList();
-
-            // Trả về kết quả
-            return entities;
+            return DbConnection.Query<MISAEntity>(
+                sql: $"Proc_Get{TableName}s",
+                commandType: CommandType.StoredProcedure);
         }
 
         /// <summary>
@@ -76,17 +73,19 @@ namespace MISA.CukCuk.Infrastructure.Repositories
         /// <param name="entityId">Khóa chính (id)</param>
         /// <returns>Đối tượng</returns>
         /// CreatedBy: PTHIEU (30-07-2021)
+        /// ModifiedBy: PTHIEU (02-08-2021)
+        /// Sử dụng Stored Procedure thay cho câu lệnh sql dạng text 
         public MISAEntity GetById(Guid entityId)
         {
-            // Khai báo lệnh sql
-            var sqlCommand = $"SELECT * FROM {_entityClassName} WHERE {_entityClassName}Id = @{_entityClassName}Id";
+            // Thiết lập tham số
             var parameters = new DynamicParameters();
-            parameters.Add($"@{_entityClassName}Id", entityId);
+            parameters.Add($"@{TableName}Id", entityId);
 
             // Thực hiện truy vấn dữ liệu với Dapper
-            var entity = DbConnection.QueryFirstOrDefault<MISAEntity>(sqlCommand, param: parameters);
-
-            return entity;
+            return DbConnection.QueryFirstOrDefault<MISAEntity>(
+                sql: $"Proc_Get{TableName}ById", 
+                param: parameters,
+                commandType: CommandType.StoredProcedure);
         }
 
         /// <summary>
@@ -94,38 +93,13 @@ namespace MISA.CukCuk.Infrastructure.Repositories
         /// </summary>
         /// <param name="entity">Đối tượng cần thêm</param>
         /// <returns>Số bản ghi được thêm</returns>
-        /// CreatedBy: PTHIEU (2/8/2021)
-        public int Add(MISAEntity entity)
+        /// CreatedBy: PTHIEU (02/08/2021)
+        public int Insert(MISAEntity entity)
         {
-            var properties = entity.GetType().GetProperties();
-            var columns = string.Empty;
-            var values = string.Empty;
-
-            DynamicParameters parameters = new DynamicParameters();
-
-            foreach(var prop in properties)
-            {
-                var propName = prop.Name;
-                if(propName.ToLower() == $"{_entityClassName}Id".ToLower() && (prop.PropertyType == typeof(Guid) || prop.PropertyType == typeof(Guid?)))
-                {
-                    prop.SetValue(entity, Guid.NewGuid());
-                }
-
-                var propValue = prop.GetValue(entity);
-
-                columns = $"{columns} {propName},";
-                values = $"{values} @{propName},";
-                parameters.Add($"@{propName}", propValue);
-            }
-
-            columns = columns.Remove(columns.Length - 1, 1);
-            values = values.Remove(values.Length - 1, 1);
-
-            var sqlCommand = $"INSERT INTO {_entityClassName}({columns}) VALUES ({values});";
-            var rowAffects = DbConnection.Execute(sqlCommand, param: parameters, commandType: CommandType.Text);
-
-
-            return rowAffects;
+            return DbConnection.Execute(
+                sql: $"Proc_Insert{TableName}", 
+                param: entity, 
+                commandType: CommandType.StoredProcedure);
         }
 
         /// <summary>
@@ -133,35 +107,13 @@ namespace MISA.CukCuk.Infrastructure.Repositories
         /// </summary>
         /// <param name="entity">Đối tượng cần cập nhật thông tin</param>
         /// <returns>Số bản ghi bị ảnh hưởng</returns>
-        /// CreatedBy: PTHIEU (2/8/2021)
+        /// CreatedBy: PTHIEU (02/08/2021)
         public int Update(MISAEntity entity)
         {
-            var properties = entity.GetType().GetProperties();
-            var values = string.Empty;
-
-            DynamicParameters parameters = new DynamicParameters();
-            //parameters.Add($"@{_entityClassName}Id", entityId);
-            foreach (var prop in properties)
-            {
-                var propName = prop.Name;
-                var propValue = prop.GetValue(entity);
-
-                //if (propName.ToLower() == $"{_entityClassName}Id".ToLower())
-                //{
-                //    continue;
-                //}
-
-                values = $"{values} {propName} = @{propName},";
-                parameters.Add($"@{propName}", propValue);
-            }
-
-            values = values.Remove(values.Length - 1, 1);
-
-            var sqlCommand = $"UPDATE {_entityClassName} SET {values} WHERE {_entityClassName}Id = @{_entityClassName}Id;";
-            var rowAffects = DbConnection.Execute(sqlCommand, param: parameters, commandType: CommandType.Text);
-
-
-            return rowAffects;
+            return DbConnection.Execute(
+               sql: $"Proc_Update{TableName}",
+               param: entity, 
+               commandType: CommandType.StoredProcedure);
         }
 
         /// <summary>
@@ -169,17 +121,88 @@ namespace MISA.CukCuk.Infrastructure.Repositories
         /// </summary>
         /// <param name="entityId">Khóa chính</param>
         /// <returns>Số bản ghi bị ảnh hưởng</returns>
+        /// CreatedBy: PTHIEU (30-07-2021)
+        /// ModifiedBy: PTHIEU (02-08-2021)
+        /// Sử dụng Stored Procedure thay cho câu lệnh sql dạng text 
         public int Delete(Guid entityId)
         {
-            // Khai báo lệnh sql
-            var sqlCommand = $"DELETE FROM {_entityClassName} WHERE {_entityClassName}Id = @{_entityClassName}Id";
+            // Thiết lập tham số
             var parameters = new DynamicParameters();
-            parameters.Add($"@{_entityClassName}Id", entityId);
+            parameters.Add($"@{TableName}Id", entityId);
 
             // Thực hiện câu lệnh sql
-            var rowAffected = DbConnection.Execute(sqlCommand, param: parameters);
+            return DbConnection.Execute(
+                sql: $"Proc_Delete{TableName}ById", 
+                param: parameters,
+                commandType: CommandType.StoredProcedure);
+        }
 
-            return rowAffected;
+        /// <summary>
+        /// Hàm kiểm tra trùng lặp dữ liệu
+        /// </summary>
+        /// <param name="propName">Tên thuộc tính (tương ứng với tên trường trong CSDL)</param>
+        /// <param name="value">Giá trị muốn kiểm tra</param>
+        /// <returns>true - giá trị bị trùng, false - giá trị không bị trùng</returns>
+        /// CreatedBy: PTHIEU (2/8/2021)
+        public bool CheckDuplicate(string propName, object value)
+        {
+            // Khai báo câu lệnh sql
+            var sqlCommand = $"SELECT * FROM {TableName} WHERE {propName} = @{propName}";
+
+            // Thiết lập tham số
+            var parameters = new DynamicParameters();
+            parameters.Add($"@{propName}", value);
+
+            // Thực hiện truy vấn
+            var result = DbConnection.QueryFirstOrDefault<object>(
+                sql: sqlCommand, 
+                param: parameters, 
+                commandType: CommandType.Text);
+
+            // Nếu có dữ liệu trả về (khác null) => giá trị prop bị trùng
+            if(result != null)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Hàm kiểm tra trùng lặp dữ liệu trước khi update bản ghi
+        /// </summary>
+        /// <param name="propName">Tên trường dữ liệu</param>
+        /// <param name="value">Giá trị cần kiểm tra</param>
+        /// <param name="entityId">Đối tượng thực thể</param>
+        /// <returns>true - giá trị bị trùng, false - giá trị không bị trùng</returns>
+        /// CreatedBy: PTHIEU (2/8/2021)
+        public bool CheckDuplicateBeforeUpdate(string propName, object value, MISAEntity entity)
+        {
+            var entityId = typeof(MISAEntity).GetProperty($"{TableName}Id").GetValue(entity);
+
+            // Khai báo câu lệnh sql
+            // Có thể dùng: AND hoặc &&, <> hoặc != 
+            var sqlCommand = $"SELECT * FROM {TableName} WHERE {TableName}Id <> @{TableName}Id AND {propName} = @{propName}";
+
+            // Thiết lập tham số
+            var parameters = new DynamicParameters();
+            parameters.Add($"@{propName}", value);
+            parameters.Add($"@{TableName}Id", entityId);
+
+            // Thực hiện truy vấn
+            var result = DbConnection.QueryFirstOrDefault<object>(
+                sql: sqlCommand,
+                param: parameters,
+                commandType: CommandType.Text);
+
+
+            // Nếu có dữ liệu trả về (khác null) => giá trị prop bị trùng
+            if (result != null)
+            {
+                return true;
+            }
+
+            return false;
         }
         #endregion
     }
